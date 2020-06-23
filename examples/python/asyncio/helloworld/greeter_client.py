0b63976ab1e6bf2cb716f2ebff58f5d430051320
Richard Belleville <j.belleville.richard@gmail.com>
2020-06-22 22:05:53 -0700

    WIP. Should be working

---
+++ examples/python/asyncio/helloworld/greeter_client.py
@@ -0,0 +1,36 @@
+# Copyright 2020 The gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python asyncio implementation of the gRPC helloworld.Greeter client."""
+
+import asyncio
+import logging
+import grpc.experimental.aio as grpc_aio
+
+from helloworld_pb2 import HelloRequest
+from helloworld_pb2_grpc import GreeterStub
+
+
+async def run_client():
+    async with grpc_aio.insecure_channel('localhost:50051') as channel:
+        print("Creating stub")
+        stub = GreeterStub(channel)
+        print("Awaiting response")
+        response = await stub.SayHello(HelloRequest(name="you"))
+        print("Finished awaiting response.")
+    print(f"Greeter client received: {response.message}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    asyncio.run(run_client())
---
+++ examples/python/asyncio/helloworld/greeter_server.py
@@ -0,0 +1,46 @@
+# Copyright 2020 The gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python asyncio implementation of the gRPC helloworld.Greeter server."""
+
+import asyncio
+import logging
+# TODO: Shorten
+import grpc.experimental.aio
+
+grpc.experimental.aio.init_grpc_aio()
+
+from helloworld_pb2 import HelloReply
+from helloworld_pb2_grpc import GreeterServicer, add_GreeterServicer_to_server
+
+
+class Greeter(GreeterServicer):
+
+    # TODO: Type annotations.
+    async def SayHello(self, request, context):
+        print("Received request")
+        return HelloReply(message=f"Hello {request.name}")
+
+
+async def serve():
+    server = grpc.experimental.aio.server()
+    add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('localhost:50051')
+    await server.start()
+    print("Server started")
+    await server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(serve())
