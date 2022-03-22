83b70e7beb15423134e9c72cb75b285becef83c1
Richard Belleville <rbellevi@google.com>
2022-03-22 14:31:04 -0700

    Create single source of truth for Bazel versions

--- test/distrib/bazel/run_bazel_distrib_test.sh
+++ test/distrib/bazel/run_bazel_distrib_test.sh
@@ -17,17 +17,11 @@ set -ex
 
 cd "$(dirname "$0")"
 
-# TODO(jtattermusch): make build work with bazel 2.2.0 and bazel 1.2.1 if that's reasonably simple.
-# TODO(rbellevi): Create a single source of truth for this and the support
-# markdown doc.
-SUPPORTED_VERSIONS=(
-  "3.7.2"
-  "4.2.2"
-  "5.0.0"
-)
+# Ignore comment lines.
+VERSIONS=$(cat supported_versions.txt | grep -v '#')
 
 FAILED_VERSIONS=""
-for VERSION in "${SUPPORTED_VERSIONS[@]}"; do
+for VERSION in ; do
     echo "Running bazel distribtest with bazel version ${VERSION}"
     ./test_single_bazel_version.sh "${VERSION}" || FAILED_VERSIONS="${FAILED_VERSIONS}${VERSION} "
 done
