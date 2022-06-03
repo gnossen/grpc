156b907303903ff8a25211f60fd2f4f73662a91b
gnossen <gnossen@users.noreply.github.com>
2022-06-03 00:51:59 +0000

    Automated change: Fix sanity tests

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ import _metadata
 import pkg_resources
 from setuptools.command import egg_info
 
-
 os.environ['ARCHFLAGS'] = '-arch {}'.format(platform.machine())
 
 # Redirect the manifest template from MANIFEST.in to PYTHON-MANIFEST.in.
@@ -429,8 +428,10 @@ if "linux" in sys.platform or "darwin" in sys.platform:
 if 'darwin' in sys.platform:
     # os.environ['ARCHFLAGS'] = '-arch {}'.format(platform.machine())
     import distutils.sysconfig
-    sys.stderr.write("AAAAAAAAAAAAAAAAAAAA ARCHFLAGS: '{}'\n".format(os.environ['ARCHFLAGS']))
-    sys.stderr.write("distutils.sysconfig.get_config_vars: '{}'\n".format(distutils.sysconfig.get_config_vars()))
+    sys.stderr.write("AAAAAAAAAAAAAAAAAAAA ARCHFLAGS: '{}'\n".format(
+        os.environ['ARCHFLAGS']))
+    sys.stderr.write("distutils.sysconfig.get_config_vars: '{}'\n".format(
+        distutils.sysconfig.get_config_vars()))
     sys.stderr.flush()
     # sys.exit(1)
     mac_target = sysconfig.get_config_var('MACOSX_DEPLOYMENT_TARGET')
